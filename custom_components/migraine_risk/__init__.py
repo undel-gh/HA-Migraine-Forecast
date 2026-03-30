3fb5e37209c87ee1bc978628db019d9d35e9cdbb
undel-gh <dnesterkin@gmail.com>
2026-03-31 01:31:07 +0300

    Update __init__.py

--- custom_components/migraine_risk/__init__.py
+++ custom_components/migraine_risk/__init__.py
@@ -1,2 +1,12 @@
+"""Migraine Risk integration."""
+from homeassistant.helpers import entity_platform
+
+DOMAIN = "migraine_risk"
+
 async def async_setup(hass, config):
+    """Set up the Migraine Risk integration."""
+    return True
+
+async def async_setup_entry(hass, entry):
+    """Not used; we are YAML only."""
     return True
